b11c77819ff441bf3637d207ba18d9ad1c9f7342
Michael Owuor <michael.owuor@gmail.com>
2022-04-16 08:25:14 +0300

    feat: fetching data (getStaticProps).

--- pages/mandos/index.js
+++ pages/mandos/index.js
@@ -1,7 +1,23 @@
-const Mandos = () => {
+export const getStaticProps = async () => {
+	const res = await fetch('http://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+
+	return {
+		props: { mandos: data },
+	};
+};
+
+const Mandos = ({ mandos }) => {
 	return (
-		<div>
-			<h1>All Mandalorians</h1>
+		<div className="p-3">
+			<h1 className="pb-5 font-semibold text-2xl">All Mandalorians</h1>
+			{mandos.map((mando) => (
+				<div key={mando.id}>
+					<a className="py-[10px] px-4 bg-white block my-5 mx-[10px] border-l-8 hover:border-l-sky-800">
+						<h3>{mando.name}</h3>
+					</a>
+				</div>
+			))}
 		</div>
 	);
 };

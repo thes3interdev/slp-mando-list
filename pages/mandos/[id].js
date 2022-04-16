50ec88833b20559ff4c4059cea8dbae2df291ca4
Michael Owuor <michael.owuor@gmail.com>
2022-04-16 08:59:34 +0300

    feat: feting a single item.

--- pages/mandos/[id].js
+++ pages/mandos/[id].js
@@ -13,10 +13,38 @@ export const getStaticPaths = async () => {
 	};
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	const res = await fetch(`http://jsonplaceholder.typicode.com/users/${id}`);
+	const data = await res.json();
+
+	return {
+		props: { mando: data },
+	};
+};
+
+const Details = ({ mando }) => {
 	return (
 		<div className="p-3">
-			<h1 className="pb-5 font-semibold text-2xl">Details</h1>
+			<h1 className="pb-5 font-semibold text-2xl">{mando.name}</h1>
+			<div className="space-y-3">
+				<p>
+					<span className="font-bold">Email Address: </span>
+					{mando.email}
+				</p>
+				<p>
+					<span className="font-bold">Website: </span>
+					{mando.website}
+				</p>
+				<p>
+					<span className="font-bold">City of Residence: </span>
+					{mando.address.city}
+				</p>
+				<p>
+					<span className="font-bold">Employer: </span>
+					{mando.company.name}
+				</p>
+			</div>
 		</div>
 	);
 };

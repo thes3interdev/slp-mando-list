60c57af63a47b2eb9981d898bfec3b69c641a136
Michael Owuor <michael.owuor@gmail.com>
2022-04-16 08:36:31 +0300

    feat: creating dynamic routes.

---
+++ pages/mandos/[id].js
@@ -0,0 +1,9 @@
+const Details = () => {
+	return (
+		<div className="p-3">
+			<h1 className="pb-5 font-semibold text-2xl">Details</h1>
+		</div>
+	);
+};
+
+export default Details;
--- pages/mandos/index.js
+++ pages/mandos/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export const getStaticProps = async () => {
 	const res = await fetch('http://jsonplaceholder.typicode.com/users');
 	const data = await res.json();
@@ -12,11 +14,11 @@ const Mandos = ({ mandos }) => {
 		<div className="p-3">
 			<h1 className="pb-5 font-semibold text-2xl">All Mandalorians</h1>
 			{mandos.map((mando) => (
-				<div key={mando.id}>
+				<Link href={`/mandos/${mando.id}`} key={mando.id}>
 					<a className="py-[10px] px-4 bg-white block my-5 mx-[10px] border-l-8 hover:border-l-sky-800">
 						<h3>{mando.name}</h3>
 					</a>
-				</div>
+				</Link>
 			))}
 		</div>
 	);

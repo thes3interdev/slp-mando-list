0d95c9ed0463d633346c100e44a4e9b64799c6fe
Michael Owuor <michael.owuor@gmail.com>
2022-04-16 08:45:00 +0300

    feat: dynamic routes (getStaticPaths).

--- pages/mandos/[id].js
+++ pages/mandos/[id].js
@@ -1,3 +1,18 @@
+export const getStaticPaths = async () => {
+	const res = await fetch('http://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+	const paths = data.map((mando) => {
+		return {
+			params: { id: mando.id.toString() },
+		};
+	});
+
+	return {
+		paths,
+		fallback: false,
+	};
+};
+
 const Details = () => {
 	return (
 		<div className="p-3">
